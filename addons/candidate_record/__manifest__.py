caea9b65aa9d72af4983fe9210c73420a091b011
thineisan97 <umgodoodev9@gmail.com>
2023-01-26 15:44:43 +0630

    first commit

---
+++ addons/candidate_record/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name' : 'Candidate Record',
+    'version' : '0.1',
+    
+    'license' : 'LGPL-3',
+    'description' : """Fundamental Project""",
+
+    'depends' : [
+        'base',
+    ],
+    'data' : [
+        'security/ir.model.access.csv',
+        'views/ar_assign_view.xml',
+        'views/ar_configuration_view.xml',
+        'views/ar_repayment_view.xml'
+    ],
+    # 'assets':{
+    #     'web.assets_backend':[
+    #         '/home/eisan/user/src/default/project/addons/hi5_fa/static/src/css/style.css'
+    #         ]
+
+    # }
+    
+}
